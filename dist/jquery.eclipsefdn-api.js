2bf7dc75e7985b723df7f3e96aacdc6092689fed
Christopher Guindon <chris.guindon@eclipse.org>
2016-10-31 10:36:20 -0400

    prepare for 0.0.3 release
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- dist/jquery.eclipsefdn-api.js
+++ dist/jquery.eclipsefdn-api.js
@@ -1,5 +1,5 @@
 /*
- *  jquery-eclipsefdn-api - v0.0.2
+ *  jquery-eclipsefdn-api - v0.0.3
  *  Fetch and display data from various Eclipse Foundation APIs.
  *  https://github.com/EclipseFdn/jquery-eclipsefdn-api
  *
