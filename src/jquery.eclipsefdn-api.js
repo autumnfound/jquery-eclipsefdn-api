b7adde9a191d0d63e9c77acccb45e293c72c1f43
Christopher Guindon <chris.guindon@eclipse.org>
2016-11-30 17:47:05 -0500

    update forum link
    Change-Id: I9567f74936e361cd205828f54db6251aee66396b
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- src/jquery.eclipsefdn-api.js
+++ src/jquery.eclipsefdn-api.js
@@ -163,7 +163,7 @@
               current_user_id: data.id
             };
             more_forums_link.attr({
-              "href": self.settings.forumsUrl + "/index.php/u/" + request_data.current_user_id + "/",
+              "href": self.settings.forumsUrl + "/index.php/sp/" + request_data.current_user_id + "/",
             });
             
             tr = $("<tr></tr>");
@@ -215,7 +215,7 @@
 
             // Last message column
             var last_message = $("<small></small>").append(self.dateFormat(new Date(parseInt(request_data.last_message_timestamp * 1000)))).append("<br/> By: ").append(a.clone().attr({
-              "href": self.settings.forumsUrl + "/index.php/u/" + request_data.last_message_poster_id + "/"
+              "href": self.settings.forumsUrl + "/index.php/sp/" + request_data.last_message_poster_id + "/"
             }).text(request_data.last_message_poster_alias));
             tr.append(td.clone().html(last_message).attr("class", "text-center"));
 
