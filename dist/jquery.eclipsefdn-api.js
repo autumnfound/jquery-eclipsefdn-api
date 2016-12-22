1516a937048451d2792b6acebac72e1c960c0133
Christopher Guindon <chris.guindon@eclipse.org>
2016-12-22 11:49:05 -0500

    update to 0.0.11
    Change-Id: Ibeebcf8f0140670fbe419e714381bc1b9a1a1897
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse.org>

--- dist/jquery.eclipsefdn-api.js
+++ dist/jquery.eclipsefdn-api.js
@@ -1,5 +1,5 @@
 /*
- *  jquery-eclipsefdn-api - v0.0.10
+ *  jquery-eclipsefdn-api - v0.0.11
  *  Fetch and display data from various Eclipse Foundation APIs.
  *  https://github.com/EclipseFdn/jquery-eclipsefdn-api
  *
