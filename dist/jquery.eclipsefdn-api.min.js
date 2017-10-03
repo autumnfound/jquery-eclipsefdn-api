de8f5739214edaef3765a5be2d5c432f8a7115cc
Eric Poirier <eric.poirier@eclipse-foundation.org>
2017-10-03 15:30:06 -0400

    prepare for 0.0.18
    Change-Id: Ic20a91eee4d1e2ca5ff298f92deeded2a0866e8b
    Signed-off-by: Eric Poirier <eric.poirier@eclipse-foundation.org>

--- dist/jquery.eclipsefdn-api.min.js
+++ dist/jquery.eclipsefdn-api.min.js
@@ -1,5 +1,5 @@
 /*
- *  jquery-eclipsefdn-api - v0.0.17
+ *  jquery-eclipsefdn-api - v0.0.18
  *  Fetch and display data from various Eclipse Foundation APIs.
  *  https://github.com/EclipseFdn/jquery-eclipsefdn-api
  *
