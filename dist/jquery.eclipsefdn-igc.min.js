e72198724c398dd4c311421928a08b0ecece068f
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2020-04-23 16:22:52 -0400

    Update to jquery 3.3.1
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- dist/jquery.eclipsefdn-igc.min.js
+++ dist/jquery.eclipsefdn-igc.min.js
@@ -1,5 +1,5 @@
 /*
- *  jquery-eclipsefdn-api - v0.0.24
+ *  jquery-eclipsefdn-api - v0.0.25
  *  Fetch and display data from various Eclipse Foundation APIs.
  *  https://github.com/EclipseFdn/jquery-eclipsefdn-api
  *
--- karma.conf.js
+++ karma.conf.js
@@ -5,7 +5,7 @@ process.env.CHROME_BIN = puppeteer.executablePath();
 module.exports = function( config ) {
 	config.set( {
 		files: [
-			"node_modules/jquery/jquery.js",
+			"node_modules/jquery/dist/jquery.min.js",
 			"dist/jquery.eclipsefdn-api.js",
 			"test/setup.js",
 			"test/spec/*"
