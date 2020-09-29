7543374d28c68523ae1f4933af77c185666c882a
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2020-09-29 15:25:46 -0400

    0.0.36

--- dist/jquery.eclipsefdn-igc.js
+++ dist/jquery.eclipsefdn-igc.js
@@ -1,5 +1,5 @@
 /*
- *  jquery-eclipsefdn-api - v0.0.35
+ *  jquery-eclipsefdn-api - v0.0.36
  *  Fetch and display data from various Eclipse Foundation APIs.
  *  https://github.com/EclipseFdn/jquery-eclipsefdn-api
  *
