2f2c50b74768129f3d085d09907409692abf20a1
Christopher Guindon <chris.guindon@eclipse-foundation.org>
2021-01-06 14:07:22 -0500

    0.0.44
    Signed-off-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

--- dist/jquery.eclipsefdn-api.min.js
+++ dist/jquery.eclipsefdn-api.min.js
@@ -1,5 +1,5 @@
 /*
- *  jquery-eclipsefdn-api - v0.0.43
+ *  jquery-eclipsefdn-api - v0.0.44
  *  Fetch and display data from various Eclipse Foundation APIs.
  *  https://github.com/EclipseFdn/jquery-eclipsefdn-api
  *
